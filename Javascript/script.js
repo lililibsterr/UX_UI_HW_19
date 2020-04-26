677a696f41223ef70944f1519f01bf119f5bf199
lililibsterr <63078658+lililibsterr@users.noreply.github.com>
2020-04-26 06:09:19 -0400

    Figuring out interior thumbnails
    *Trying to get interior project thumbnails to show up properly
    *Fix images
    *Copying and pasting different JS codes. None working yet.

--- Javascript/script.js
+++ Javascript/script.js
@@ -18,73 +18,89 @@
 // });
 
 // slide menu
-function initDrag() {
-    var total = ($("#workPages > div").size() - 1);
-    var start, stop;
-    //
-    $("#workPages > div").each(function(i) {
-        if (i != "0") {
-            $(this).css('left', $("#workPages").width() + "px");
-        }
-        $("#slide_" + i + "").on("hover", function(event) {
-            previous = "slide_" + (i - 1) + "";
-            current = $(this).attr("id");
-            next = "slide_" + (i + 1) + "";
-            if (current == "slide_0") {
-                previous = "slide_" + total + "";
-            }
-            if (current == "slide_" + total + "") {
-                next = "slide_0";
-            }
-            $(this).draggable({
-                axis: "x",
-                cursor: "move",
-                start: function(event, ui) {
-                    event.stopPropagation();
-                    start = ui.position.left;
-                },
-                stop: function(event, ui) {
-                    event.stopPropagation();
-                    stop = ui.position.left;
-                    if (start > stop) {
-                        $("#" + next + "").animate({
-                            left: "-=" + $('#workPages').width() + "px"
-                        }, {
-                            duration: 'slow',
-                            easing: 'easeOutBack'
-                        });
-                        $("#" + current + "").css('left', $('#workPages').width() + "px");
-                    } else {
-                        $("#" + previous + "").css('left', "-" + ($('#workPages').width() + "px"));
-                        $("#" + previous + "").animate({
-                            left: "0"
-                        }, {});
-                        $("#" + current + "").css('left', $('#workPages').width() + "px");
-                    }
-                }
-            });
-        });
-    });
-    timer = true;
-}
-//
-$('.menu > li').find('a').on('click', function(event) {
-    event.preventDefault();
-    $("#workPages > div").each(function(i) {
-        if ($(this).css('left') == "0px") {
-            on_screen = $(this).attr('id');
-        }
-    });
-    scroll_element = $(this).attr('href');
-    $(scroll_element).animate({
-        left: "-=" + $('#workPages').width() + "px"
-    }, {
-        duration: 'slow',
-        easing: 'easeOutBack'
-    });
-    $("#" + on_screen + "").css('left', $('#workPages').width() + "px");
-});
-//
-$(document).ready(function() {
-    initDrag();
+// function initDrag() {
+//     var total = ($("#workPages > div").size() - 1);
+//     var start, stop;
+//     //
+//     $("#workPages > div").each(function(i) {
+//         if (i != "0") {
+//             $(this).css('left', $("#workPages").width() + "px");
+//         }
+//         $("#slide_" + i + "").on("hover", function(event) {
+//             previous = "slide_" + (i - 1) + "";
+//             current = $(this).attr("id");
+//             next = "slide_" + (i + 1) + "";
+//             if (current == "slide_0") {
+//                 previous = "slide_" + total + "";
+//             }
+//             if (current == "slide_" + total + "") {
+//                 next = "slide_0";
+//             }
+//             $(this).draggable({
+//                 axis: "x",
+//                 cursor: "move",
+//                 start: function(event, ui) {
+//                     event.stopPropagation();
+//                     start = ui.position.left;
+//                 },
+//                 stop: function(event, ui) {
+//                     event.stopPropagation();
+//                     stop = ui.position.left;
+//                     if (start > stop) {
+//                         $("#" + next + "").animate({
+//                             left: "-=" + $('#workPages').width() + "px"
+//                         }, {
+//                             duration: 'slow',
+//                             easing: 'easeOutBack'
+//                         });
+//                         $("#" + current + "").css('left', $('#workPages').width() + "px");
+//                     } else {
+//                         $("#" + previous + "").css('left', "-" + ($('#workPages').width() + "px"));
+//                         $("#" + previous + "").animate({
+//                             left: "0"
+//                         }, {});
+//                         $("#" + current + "").css('left', $('#workPages').width() + "px");
+//                     }
+//                 }
+//             });
+//         });
+//     });
+//     timer = true;
+// }
+// //
+// $('.menu > li').find('a').on('click', function(event) {
+//     event.preventDefault();
+//     $("#workPages > div").each(function(i) {
+//         if ($(this).css('left') == "0px") {
+//             on_screen = $(this).attr('id');
+//         }
+//     });
+//     scroll_element = $(this).attr('href');
+//     $(scroll_element).animate({
+//         left: "-=" + $('#workPages').width() + "px"
+//     }, {
+//         duration: 'slow',
+//         easing: 'easeOutBack'
+//     });
+//     $("#" + on_screen + "").css('left', $('#workPages').width() + "px");
+// });
+// //
+// $(document).ready(function() {
+//     initDrag();
+// });
+
+$(function() {
+	$('.work-options').bind('click',function(event){
+		var $anchor = $(this);
+		
+		// if you want to use one of the easing effects:
+		$('html, body').stop().animate({
+			scrollLeft: $($anchor.attr('href')).offset().left
+		}, 1500,'easeInOutExpo');
+		
+		$('html, body').stop().animate({
+			scrollLeft: $($anchor.attr('href')).offset().left
+		}, 1000);
+		event.preventDefault();
+	});
 });
