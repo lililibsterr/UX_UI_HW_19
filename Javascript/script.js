f42c264d85c2cd276b37d3bc153294780bb46136
lililibsterr <63078658+lililibsterr@users.noreply.github.com>
2020-04-27 22:14:09 -0400

    Added smooth scrolling
    *Decided to scrap the horizontal scroll and changing it to smooth scrolling instead
    *Will change homepage to a full, longer page instead

--- Javascript/script.js
+++ Javascript/script.js
@@ -90,13 +90,13 @@
 // });
 
 $(function() {
-	$('.work-options').bind('click',function(event){
+	$('section.work-options a').bind('click',function(event){
 		var $anchor = $(this);
 		
 		// if you want to use one of the easing effects:
-		$('html, body').stop().animate({
-			scrollLeft: $($anchor.attr('href')).offset().left
-		}, 1500,'easeInOutExpo');
+		// $('html, body').stop().animate({
+		// 	scrollLeft: $($anchor.attr('href')).offset().left
+		// }, 1500,'easeInOutExpo');
 		
 		$('html, body').stop().animate({
 			scrollLeft: $($anchor.attr('href')).offset().left
